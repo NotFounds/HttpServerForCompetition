bb014c32dd5306e516d87b2cb3d95ff8bd350d27
NotFounds <m134_warrock_rpg7@yahoo.co.jp>
2015-11-21 18:02:30 +0900

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,15 +1,18 @@
 if "%~x1" == ".c" (
+	Set PATH = %PATH%;C:\borland\bcc55\Bin;
 	if exist C:\borland\bcc55\Bin (
 		bcc32 -n%~dp1 %1
 	)
 )
 
 if "%~x1" == ".cpp" (
+	Set PATH = %PATH%;C:\borland\bcc55\Bin;
 	if exist C:\borland\bcc55\Bin (
 		bcc32 -n%~dp1 %1
 	)
 )
 
 if "%~x1" == ".cs" (
+	Set PATH = %PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319;
 	csc /out:%~dp1%~n1.exe %1
 )
