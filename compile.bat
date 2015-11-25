07ec33c2eb4e21516be4676e67eeedb748b87d9b
NotFounds <m134_warrock_rpg7@yahoo.co.jp>
2015-11-25 20:18:43 +0900

    Fix Bug

--- compile.bat
+++ compile.bat
@@ -1,18 +1,19 @@
+@echo off
 if "%~x1" == ".c" (
-	Set PATH = %PATH%;C:\borland\bcc55\Bin;
+	PATH = C:\borland\bcc55\Bin
 	if exist C:\borland\bcc55\Bin (
 		bcc32 -n%~dp1 %1
 	)
 )
 
 if "%~x1" == ".cpp" (
-	Set PATH = %PATH%;C:\borland\bcc55\Bin;
+	Set PATH = C:\borland\bcc55\Bin
 	if exist C:\borland\bcc55\Bin (
 		bcc32 -n%~dp1 %1
 	)
 )
 
 if "%~x1" == ".cs" (
-	Set PATH = %PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319;
+	Set PATH = C:\Windows\Microsoft.NET\Framework\v4.0.30319
 	csc /out:%~dp1%~n1.exe %1
 )
